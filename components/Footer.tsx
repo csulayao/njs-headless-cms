c90e21d6782454dcb396492c64a79a345ac6c85e
csulayao <csulayao.jnj@gmail.com>
2025-01-05 01:07:32 +0800

    Updated footer code base to base off of portfolia.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,29 +1,37 @@
+'use client'
+import Link from 'next/link';
+import Image from 'next/image';
+
 const Footer = () => {
   return (
-    <footer className="bg-accent-1 border-t border-accent-2">
-      <div className="container mx-auto px-5">
-        <div className="py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-5xl font-bold tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4 lg:w-1/2">
-            Built with Next.js.
-          </h3>
-          <div className="flex flex-col lg:flex-row justify-center items-center lg:pl-4 lg:w-1/2">
-            <a
-              href="https://nextjs.org/docs"
-              className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
-            >
-              Read Documentation
-            </a>
-            <a
-              href=""
-              className="mx-3 font-bold hover:underline"
-            >
-              View on GitHub
-            </a>
-          </div>
-        </div>
+    <footer className="flex w-full mb-0 pt-5 pb-5 pl-10 bg-slate-950 text-white font-satoshi">
+      {/* Desktop */}
+      <div className="sm:flex hidden md:flex-1">
+        <span className="flex mr-10 mt-7">&copy; D-Kolektiv, Inc. 2024 <br /><br />
+        </span>
       </div>
+        <div className="sm:flex hidden md:flex-grow-0">
+          <span className="flex mr-10 mt-8 text-xs">Logos and copyright used for reference belong to their respective brands owners.</span>
+        <Link href="https://www.linkedin.com/in/carlosulayao" className="flex mr-5 mt-5" rel="noopener noreferrer" target="_blank">
+          <Image src="https://portfolia-dusky.vercel.app/assets/icons/linkedin.svg" 
+          alt="LinkedIn" width={28} height={28} className="mr-3" unoptimized={true} loader={() => "https://portfolia-dusky.vercel.app/assets/icons/linkedin.svg"}/>
+          Test
+        </Link>
+        {/* <Link href="https://github.com/csulayao" className="flex mr-10 mt-5" rel="noopener noreferrer" target="_blank"><Image src="assets/icons/github.svg" alt="Github" width={30} height={30} className="mr-3"/></Link> */}
+        </div>
+      {/* Mobile */}
+      {/* <div className="sm:hidden flex">
+            <div className="w-full">
+              <span className="w-full">&copy; D-Kolektiv, Inc. 2024 <br /><br />
+              </span>
+
+            </div>
+            <div className="w-full">
+                <span className="text-xs">Logos and copyright used for reference belong to their respective brand owners.</span>
+            </div>
+      </div> */}
     </footer>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
