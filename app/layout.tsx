05c38179282797bb106bf4549deb57b0f880332d
csulayao <csulayao.jnj@gmail.com>
2025-01-05 01:06:56 +0800

    Added the footer component.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 // import { EXAMPLE_PATH, CMS_NAME } from "@/lib/constants";
 import Footer from "@/components/Footer";
+import Nav from "@/components/Nav";
 
 export const metadata = {
   title: `Next.js and Contentful Example`,
@@ -28,11 +29,16 @@ export default function RootLayout({
         <link rel="apple-touch-icon" href="/apple-icon.png?<generated>" type="image/<generated>" sizes="<generated>"/>
       </head>
       <body>
-
-        <section className="min-h-screen">
-          <main>{children}</main>
-          <Footer />
-        </section>
+      <section>
+      <div className="main">
+          <div className="gradient" />
+      </div>
+      <main className="app">
+          <Nav/>
+          {children}
+      </main>
+      </section>
+      <Footer />
       </body>
     </html>
   );
