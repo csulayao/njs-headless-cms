2f6dc869f9013caf9141bb2327d9ead0828b822b
csulayao <csulayao.jnj@gmail.com>
2025-01-05 01:08:03 +0800

    Configured image src referencing for remote images.

--- next.config.js
+++ next.config.js
@@ -3,5 +3,12 @@ module.exports = {
   images: {
     loader: "custom",
     formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "portfolia-dusky.vercel.app",
+        pathname: "**",
+      },
+    ],
   },
 };
