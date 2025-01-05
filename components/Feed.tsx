7e9d966af19d29cc5d9c855c3846a50e01c44b4f
csulayao <csulayao.jnj@gmail.com>
2025-01-05 22:09:57 +0800

    Added Feeds.

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,8 +1,51 @@
-import React from 'react'
+"use client";
+
+import { useState, useEffect } from 'react'
+
+import WorkCard from './WorkCard';
+
+type WorkCardListProps = {
+  data: string,
+  handleTagClick: string
+}
+
+const WorkCardList = ({data, handleTagClick}:WorkCardListProps) => {
+  return (
+    <div className="mt-5 work_layout cursor-pointer">
+      {data.map((post) => (
+        <WorkCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick}
+        />
+      ))}
+      {/* {data.map((post) => (
+        JSON.stringify(post)
+      ))} */}
+    </div>
+  )
+}
 
 const Feed = () => {
+  const [posts, setPosts] = useState([]);
+
+useEffect(() => {
+  const fetchPosts = async () => {
+    const response = await fetch('/api/works');
+    const data = await response.json();
+
+    setPosts(data);
+  }
+  fetchPosts();
+}, []);
+
   return (
-    <div>Feed</div>
+    <section className="feed">
+      <WorkCardList 
+        data={posts}
+      />
+
+    </section>
   )
 }
 
