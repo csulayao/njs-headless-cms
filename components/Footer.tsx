9fa698f36faca10e9a2078ab0306138fe39d33dd
csulayao <csulayao.jnj@gmail.com>
2025-01-05 22:06:10 +0800

    Fixed footer images. Will need to make img to be dynamic.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,10 +13,10 @@ const Footer = () => {
         <div className="sm:flex hidden md:flex-grow-0">
           <span className="flex mr-10 mt-8 text-xs">Logos and copyright used for reference belong to their respective brands owners.</span>
         <Link href="https://www.linkedin.com/in/carlosulayao" className="flex mr-5 mt-5" rel="noopener noreferrer" target="_blank">
-          <Image src="https://portfolia-dusky.vercel.app/assets/icons/linkedin.svg" 
-          alt="LinkedIn" width={28} height={28} className="mr-3" unoptimized={true} loader={() => "https://portfolia-dusky.vercel.app/assets/icons/linkedin.svg"}/>
+          <Image src="https://images.ctfassets.net/oxmmme2a1mec/5k17trG5fAgk1RiSLbfIzj/db14f305f653073acd1858a3cebd4143/linkedin.svg" 
+          alt="LinkedIn" width={28} height={28} className="mr-3" unoptimized={true} loader={() => "https://images.ctfassets.net/oxmmme2a1mec/5k17trG5fAgk1RiSLbfIzj/db14f305f653073acd1858a3cebd4143/linkedin.svg"}/>
         </Link>
-        <Link href="https://github.com/csulayao" className="flex mr-10 mt-5" rel="noopener noreferrer" target="_blank"><Image src="https://portfolia-dusky.vercel.app/assets/icons/github.svg" alt="Github" width={30} height={30} className="mr-3" unoptimized={true} loader={() => "https://portfolia-dusky.vercel.app/assets/icons/github.svg"}/></Link>
+        <Link href="https://github.com/csulayao" className="flex mr-10 mt-5" rel="noopener noreferrer" target="_blank"><Image src="https://images.ctfassets.net/oxmmme2a1mec/4vrivkZnC1ySFVEf2PAa0S/a4013183a51d2e8b961987b669ef9b57/github.svg" alt="Github" width={30} height={30} className="mr-3" unoptimized={true} loader={() => "https://images.ctfassets.net/oxmmme2a1mec/4vrivkZnC1ySFVEf2PAa0S/a4013183a51d2e8b961987b669ef9b57/github.svg"}/></Link>
         </div>
       {/* Mobile */}
       <div className="sm:hidden flex">
