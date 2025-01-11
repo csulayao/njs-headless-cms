56c7263274adbe67cf9394cdcdd7506922cf2e65
csulayao <csulayao.jnj@gmail.com>
2025-01-12 00:52:37 +0800

    Updated nav items to be dynamic based on contentful content.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,11 +2,16 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { useState, useEffect } from 'react';
+//import { useState, useEffect } from 'react';
+import { getAllMenuItems, getAllPosts } from "@/lib/api";
 
-const Nav = () => {
 
-    const [toggleDropDown, setToggleDropDown] = useState(false);
+export default async function Nav(){
+
+    //const [toggleDropDown, setToggleDropDown] = useState(false);
+    const allMenu = await getAllMenuItems();
+    const allItems = allMenu
+    //console.log(allItems)
 
   return (
     <nav className="flex-between w-full mb-4 pb-5 pt-3 bg-white sticky top-0 z-40">
@@ -19,8 +24,22 @@ const Nav = () => {
         {/* Desktop Navigation */}
         <div className="sm:flex hidden">
             <div className="flex gap-3 md:gap-5">
-                <Link className="icon_btn" href="https://www.linkedin.com/in/carlosulayao" rel="noopener noreferrer" target="_blank"><Image src="https://images.ctfassets.net/oxmmme2a1mec/5k17trG5fAgk1RiSLbfIzj/db14f305f653073acd1858a3cebd4143/linkedin.svg" alt="LinkedIn" width={28} height={28}className="mr-3" unoptimized={true} loader={() => "https://images.ctfassets.net/oxmmme2a1mec/5k17trG5fAgk1RiSLbfIzj/db14f305f653073acd1858a3cebd4143/linkedin.svg"}/>LinkedIn</Link>
-                <Link className="icon_btn mr-5" href="https://github.com/csulayao" rel="noopener noreferrer" target="_blank"><Image src="https://images.ctfassets.net/oxmmme2a1mec/4vrivkZnC1ySFVEf2PAa0S/a4013183a51d2e8b961987b669ef9b57/github.svg" alt="Github" width={30} height={30} className="mr-3" unoptimized={true} loader={() => "https://images.ctfassets.net/oxmmme2a1mec/4vrivkZnC1ySFVEf2PAa0S/a4013183a51d2e8b961987b669ef9b57/github.svg"}/> GitHub</Link>
+                {allItems.map((items) => (
+                <Link className="icon_btn mr-5" key={items.navId} href={items.navLink} rel="noopener noreferrer" title={items.navAlt} target="_blank">
+                    <Image 
+                        key={items.navId}
+                        src={items.navImage.url}
+                        unoptimized={true} loader={() => `"${items.navImage}"`}
+                        width={28} 
+                        height={28}
+                        alt={items.navAlt}
+                        className="mr-3"  
+                    />
+                    &nbsp;{items.navName}
+                </Link>
+                ))}
+                {/* <Link className="icon_btn" href="https://www.linkedin.com/in/carlosulayao" rel="noopener noreferrer" target="_blank"><Image src="https://images.ctfassets.net/oxmmme2a1mec/5k17trG5fAgk1RiSLbfIzj/db14f305f653073acd1858a3cebd4143/linkedin.svg" alt="LinkedIn" width={28} height={28} className="mr-3" unoptimized={true} loader={() => "https://images.ctfassets.net/oxmmme2a1mec/5k17trG5fAgk1RiSLbfIzj/db14f305f653073acd1858a3cebd4143/linkedin.svg"}/>LinkedIn</Link>
+                <Link className="icon_btn mr-5" href="https://github.com/csulayao" rel="noopener noreferrer" target="_blank"><Image src="https://images.ctfassets.net/oxmmme2a1mec/4vrivkZnC1ySFVEf2PAa0S/a4013183a51d2e8b961987b669ef9b57/github.svg" alt="Github" width={30} height={30} className="mr-3" unoptimized={true} loader={() => "https://images.ctfassets.net/oxmmme2a1mec/4vrivkZnC1ySFVEf2PAa0S/a4013183a51d2e8b961987b669ef9b57/github.svg"}/> GitHub</Link> */}
             </div>
         </div>
 
@@ -35,4 +54,3 @@ const Nav = () => {
 )
 }
 
-export default Nav
