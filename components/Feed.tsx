e0adca09bdd622995a8178fecd98947068c002bf
csulayao <csulayao.jnj@gmail.com>
2025-01-12 01:57:02 +0800

    Cleaned up code for better reading.

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -50,31 +50,31 @@ export default async function Feed() {
       <div className="mt-5 work_layout cursor-pointer">
          {allItems.map((projects) => (
             <div className="work_card">
-      <div className="flex justify-between items-start gap-5">
-        <div className="flex justify-start items-center gap-3">
-            <div className="flex flex-col align-top">
-              <h3 className="font-satoshi font-semibold text-indigo-950">
-                {projects.projectName}
-              </h3>
-              <p className="font-inter text-sm text-gray-500 cursor-pointer">
-                <Link href={projects.projectUrl} target="_blank" rel="noopener noreferrer">{projects.projectUrl}</Link>
+              <div className="flex justify-between items-start gap-5">
+                <div className="flex justify-start items-center gap-3">
+                    <div className="flex flex-col align-top">
+                      <h3 className="font-satoshi font-semibold text-indigo-950">
+                        {projects.projectName}
+                      </h3>
+                      <p className="font-inter text-sm text-gray-500 cursor-pointer">
+                        <Link href={projects.projectUrl} target="_blank" rel="noopener noreferrer">{projects.projectUrl}</Link>
+                      </p>
+                      <Image 
+                        src={projects.projectImageCollection.items.url}
+                        width={400}
+                        height={700}
+                        alt="placeholder_image"
+                        className="pt-5"
+                        unoptimized={true} 
+                        loader={() => `"${projects.projectUrl}"`}
+                      />
+                    </div>
+                </div>
+              </div>
+              <p className="font-inter text-sm tag_gradient pt-3">
+                {projects.projectTag}
               </p>
-              <Image 
-                src={projects.projectImageCollection.items.url}
-                width={400}
-                height={700}
-                alt="placeholder_image"
-                className="pt-5"
-                unoptimized={true} 
-                loader={() => `"${projects.projectUrl}"`}
-              />
-            </div>
-        </div>
-      </div>
-      <p className="font-inter text-sm tag_gradient pt-3">
-        {projects.projectTag}
-      </p>
-    </div>
+          </div>
       ))}
     </div>
 
