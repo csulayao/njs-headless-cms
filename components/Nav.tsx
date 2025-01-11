c610efa2162cf99649b4ba5cc656a14c0e8d5d2b
csulayao <csulayao.jnj@gmail.com>
2025-01-12 01:11:06 +0800

    Updated navigation to dynamic values for mobile version.

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -44,8 +44,21 @@ export default async function Nav(){
         {/* Mobile Navigation */}
         <div className="sm:hidden flex relative">
             <div className="flex gap-3 md:gap-5">
-                <Link className="" href="https://www.linkedin.com/in/carlosulayao" rel="noopener noreferrer" target="_blank"><Image src="https://images.ctfassets.net/oxmmme2a1mec/5k17trG5fAgk1RiSLbfIzj/db14f305f653073acd1858a3cebd4143/linkedin.svg" alt="LinkedIn" width={30} height={30} unoptimized={true} loader={() => "https://images.ctfassets.net/oxmmme2a1mec/5k17trG5fAgk1RiSLbfIzj/db14f305f653073acd1858a3cebd4143/linkedin.svg"}/></Link>
-                <Link className="mr-5" href="https://github.com/csulayao" rel="noopener noreferrer" target="_blank"><Image src="https://images.ctfassets.net/oxmmme2a1mec/4vrivkZnC1ySFVEf2PAa0S/a4013183a51d2e8b961987b669ef9b57/github.svg" alt="Github" width={30} height={30} unoptimized={true} loader={() => "https://images.ctfassets.net/oxmmme2a1mec/4vrivkZnC1ySFVEf2PAa0S/a4013183a51d2e8b961987b669ef9b57/github.svg"}/></Link>
+                {allItems.map((items) => (
+                <Link className="mr-5" key={items.navId} href={items.navLink} rel="noopener noreferrer" title={items.navAlt} target="_blank">
+                    <Image 
+                        key={items.navId}
+                        src={items.navImage.url}
+                        unoptimized={true} loader={() => `"${items.navImage}"`}
+                        width={30} 
+                        height={30}
+                        alt={items.navAlt}
+                        className="mr-3"  
+                    />
+                </Link>
+                ))}
+                {/* <Link className="" href="https://www.linkedin.com/in/carlosulayao" rel="noopener noreferrer" target="_blank"><Image src="https://images.ctfassets.net/oxmmme2a1mec/5k17trG5fAgk1RiSLbfIzj/db14f305f653073acd1858a3cebd4143/linkedin.svg" alt="LinkedIn" width={30} height={30} unoptimized={true} loader={() => "https://images.ctfassets.net/oxmmme2a1mec/5k17trG5fAgk1RiSLbfIzj/db14f305f653073acd1858a3cebd4143/linkedin.svg"}/></Link>
+                <Link className="mr-5" href="https://github.com/csulayao" rel="noopener noreferrer" target="_blank"><Image src="https://images.ctfassets.net/oxmmme2a1mec/4vrivkZnC1ySFVEf2PAa0S/a4013183a51d2e8b961987b669ef9b57/github.svg" alt="Github" width={30} height={30} unoptimized={true} loader={() => "https://images.ctfassets.net/oxmmme2a1mec/4vrivkZnC1ySFVEf2PAa0S/a4013183a51d2e8b961987b669ef9b57/github.svg"}/></Link> */}
             </div>
         </div>
     </nav> 
