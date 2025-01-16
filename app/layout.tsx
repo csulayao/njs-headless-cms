6e5a937476bfac295836be77af2130fff083b5d4
csulayao <csulayao.jnj@gmail.com>
2025-01-16 21:41:51 +0800

    Update favico reference.

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,9 @@ export default function RootLayout({
     <html lang="en" className={inter.variable}>
       <head>
         {/* TODO: Fix the favico bit */}
-        <link rel="icon" href="/icon/?<generated>" type="image/<generated>" sizes="<generated>"/>
-        <link rel="apple-touch-icon" href="/apple-icon.png?<generated>" type="image/<generated>" sizes="<generated>"/>
+        {/* <link rel="icon" href="/icon/?<generated>" type="image/<generated>" sizes="<generated>"/>
+        <link rel="apple-touch-icon" href="/apple-icon.png?<generated>" type="image/<generated>" sizes="<generated>"/> */}
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body>
       <section>
